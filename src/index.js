0faa09b266e150ea6c467997b435d44c3f42b88d
Pedro Köhler <pedrokohlerbh@gmail.com>
2020-11-19 10:29:13 -0300

    Create coming soon page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ customElements.whenDefined("router-slot").then(async () => {
   const routerSlot = document.querySelector("router-slot");
   await routerSlot.add([
     {
+      path: "portfolio",
+      component: () => import("./pages/coming-soon"),
+      guards: [],
+    },
+    {
+      path: "resume",
+      component: () => import("./pages/coming-soon"),
+      guards: [],
+    },
+    {
+      path: "blog",
+      component: () => import("./pages/coming-soon"),
+      guards: [],
+    },
+    {
+      path: "about-me",
+      component: () => import("./pages/coming-soon"),
+      guards: [],
+    },
+    {
       path: "",
       component: () => import("./pages/home"),
       guards: [],
---
+++ src/pages/coming-soon.js
@@ -0,0 +1,60 @@
+import { LitElement, html, css } from "lit-element";
+
+import "../components/app-shell";
+import theme from "../styles/theme";
+
+export default class ComingSoonPage extends LitElement {
+  static get styles() {
+    return [
+      theme,
+      css`
+        section {
+          height: 75vh;
+          padding: var(--regular-block-spacing);
+
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+
+          box-sizing: border-box;
+        }
+
+        div {
+          padding: var(--regular-block-spacing);
+
+          margin-bottom: var(--larger-block-spacing);
+
+          border-width: 1px black;
+          border-radius: 5px;
+          box-shadow: 3px 3px 3px 3px var(--shadow-color);
+          background-color: var(--elevated-surface-color);
+        }
+
+        p {
+          text-align: left;
+        }
+
+        h1 {
+          text-align: center;
+          margin-bottom: var(--larger-block-spacing);
+        }
+      `,
+    ];
+  }
+
+  render() {
+    return html`
+      <app-shell>
+        <section>
+          <div>
+            <h1>We're working on it...</h1>
+            <p>Soon this section will be available! <br> In the meantime, keep browsing around.</p>
+          </div>
+        </section>
+      </app-shell>
+    `;
+  }
+}
+
+window.customElements.define("coming-soon-page", ComingSoonPage);
